d5f412bb47ad20b197eb8a3780f9c3f30a9c752d
Sam Allen <samallen659@gmail.com>
2023-12-01 14:14:11 +0000

    adds solution for day 1 part 2

--- 1/main.go
+++ 1/main.go
@@ -2,16 +2,32 @@ package main
 
 import (
 	"fmt"
-	"github.com/samallen659/advent_of_code2023/pkg/utils"
 	"log"
+	"regexp"
 	"strconv"
 	"strings"
+
+	"github.com/samallen659/advent_of_code2023/pkg/utils"
 )
 
 var data = utils.ReadInput("/1/input.txt")
 
+var digitStrings = []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+var digitMap = map[string]string{
+	"one":   "o1e",
+	"two":   "t2o",
+	"three": "t3e",
+	"four":  "f4r",
+	"five":  "f5e",
+	"six":   "s6x",
+	"seven": "s7n",
+	"eight": "e8t",
+	"nine":  "n9e",
+}
+
 func main() {
 	fmt.Println(part1(data))
+	fmt.Println(part2(data))
 }
 
 func part1(data string) int {
@@ -19,6 +35,39 @@ func part1(data string) int {
 
 	var numbers []int
 	for _, line := range lines {
+		chars := strings.Split(line, "")
+		var numChars []string
+		for _, char := range chars {
+			_, err := strconv.Atoi(char)
+			if err != nil {
+				continue
+			}
+			numChars = append(numChars, char)
+		}
+		num, err := strconv.Atoi(numChars[0] + numChars[len(numChars)-1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		numbers = append(numbers, num)
+	}
+
+	total := 0
+	for i := range numbers {
+		total += numbers[i]
+	}
+
+	return total
+}
+
+func part2(data string) int {
+	lines := strings.Split(data, "\n")
+
+	var numbers []int
+	for _, line := range lines {
+		for _, digit := range digitStrings {
+			r, _ := regexp.Compile(digit)
+			line = string(r.ReplaceAll([]byte(line), []byte(digitMap[digit])))
+		}
 		chars := strings.Split(line, "")
 		var numChars []string
 		for _, char := range chars {
