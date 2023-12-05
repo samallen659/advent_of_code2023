725eaffbb31add1f594ccd8edcaa7e74dad6b0c3
Sam Allen <samallen659@gmail.com>
2023-12-05 16:14:20 +0000

    adds solution for day 3 part 2

--- 3/main.go
+++ 3/main.go
@@ -23,6 +23,11 @@ type numberIndex struct {
 
 func main() {
 	data := utils.ReadInput("/3/input.txt")
+	fmt.Println(part1(data))
+	fmt.Println(part2(data))
+}
+
+func part1(data string) int {
 	lines := strings.Split(data, "\n")
 	symIdxs := getSymbolIndexes(lines)
 	numIdxs := getNumberIndexes(lines)
@@ -47,7 +52,52 @@ func main() {
 		total += num
 	}
 
-	fmt.Println(total)
+	return total
+}
+
+func part2(data string) int {
+	lines := strings.Split(data, "\n")
+	starIdxs := getStarSymbolIndexes(lines)
+	numIdxs := getNumberIndexes(lines)
+
+	total := 0
+	for _, star := range starIdxs {
+		count := 0
+		ratio := 1
+		for _, numIdx := range numIdxs {
+			if numIdx.y >= star.y-1 && numIdx.y <= star.y+1 {
+				if (numIdx.xEnd >= star.x && numIdx.xEnd <= star.x+2) || (numIdx.xStart >= star.x-1 && numIdx.xStart <= star.x+1) {
+					count++
+					num, err := strconv.Atoi(lines[numIdx.y][numIdx.xStart:numIdx.xEnd])
+					if err != nil {
+						log.Fatal(err)
+					}
+					ratio *= num
+				}
+			}
+		}
+		if count == 2 {
+			total += ratio
+		}
+	}
+
+	return total
+}
+
+func getStarSymbolIndexes(lines []string) []index {
+	var indexes []index
+	for i, line := range lines {
+		r, err := regexp.Compile(`\*`)
+		if err != nil {
+			log.Fatal(err)
+		}
+		idxs := r.FindAllStringSubmatchIndex(line, 100)
+		for _, idx := range idxs {
+			indexes = append(indexes, index{x: idx[0], y: i})
+		}
+	}
+
+	return indexes
 }
 
 func getNumberIndexes(lines []string) []numberIndex {
